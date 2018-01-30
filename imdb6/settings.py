c9764b35c64dec9b3c7913d56b88d635c1432fbc
Ubuntu <ubuntu@ip-172-31-76-55.ec2.internal>
2018-01-30 02:33:05 +0000

    production settings

--- imdb6/settings.py
+++ imdb6/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'emlac4q@ta+435md1l$^q0ry46t8pbo=o7knew8or4cw24=s#t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["54.84.234.9"]
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
